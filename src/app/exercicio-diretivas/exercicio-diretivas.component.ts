bfe03766463b97fc1ad04134a4daa63891352c49
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2022-12-28 14:16:31 -0300

    Ng for

--- src/app/exercicio-diretivas/exercicio-diretivas.component.ts
+++ src/app/exercicio-diretivas/exercicio-diretivas.component.ts
@@ -7,8 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercicioDiretivasComponent {
 
-  condition = true;
+  listaFrutas = [
+    'Maçã',
+    'Laranja',
+    'Mamão',
+    'Limão',
+    'Abacate',
+  ];
 
+  ListaCarros = [{
+    id: '1',
+    placa: 'JND-7438',
+    cor: 'Preto',
+  },
+  {
+    id: '2',
+    placa: 'DJN-3874',
+    cor: 'Green',
+  },
+  {
+    id: '3',
+    placa: 'NDJ-7438',
+    cor: 'Branco',
+  },
+  {
+    id: '4',
+    placa: 'BFG-7438',
+    cor: 'Vermelho',
+  }
+];
+  condition = true;
 
   trocarValor(){
     this.condition = !this.condition;
